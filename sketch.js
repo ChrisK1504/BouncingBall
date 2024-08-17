5f204b1e1fb8aad953da72fca1e47ec63bcc7890
ChrisK1504 <krismehilli@yahoo.com>
2024-08-17 19:03:33 +0200

    Created a simple Ball class with a "show" function to print the ball.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+class Ball {
+  constructor (x,y,diameter) {
+    this.position = createVector(x,y);
+    this.velocity = createVector(0,5);
+    this.diameter = diameter;
+  }
+  show() {
+    fill("red");
+    noStroke();
+    circle(this.position.x, this.position.y, this.diameter);
+  }
+}
+
+
+let ball;
+
 function setup() {
+  ball = new Ball(100,100,10);
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  noFill();
+  stroke(255,255,255);
+  circle(200,200,400);
+  ball.show();
+
 }
